7c8e21203e8642ac52a9e1fd14373c833efb0af3
JaeHo Song <thruthesky@gmail.com>
2017-01-03 00:29:22 +0900

    updating home main menu

--- src/pages/sonub/components/header/header.ts
+++ src/pages/sonub/components/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { App } from '../../../../providers/app';
 import { Member, MEMBER_LOGIN } from '../../../../api/philgo-api/v2/member';
 @Component({
     selector: 'sonub-header',
@@ -6,7 +7,11 @@ import { Member, MEMBER_LOGIN } from '../../../../api/philgo-api/v2/member';
 })
 export class SonubHeader {
     login: MEMBER_LOGIN = null;
-    constructor( public member: Member ) {
+    constructor(
+        public app: App,
+        public member: Member
+    ) {
+        app.menu = app.size == 'small';
         member.getLogin( x => this.login = x );
     }
     onClickHeader() {
@@ -20,4 +25,7 @@ export class SonubHeader {
         localStorage.clear();
     }
 
+    onClickMenu() {
+        this.app.menu = ! this.app.menu;
+    }
 }
