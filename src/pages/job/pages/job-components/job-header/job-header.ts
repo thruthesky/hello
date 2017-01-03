3a9b5f14e9cbebf5272b68e928e697a0c91992d9
lancelynyrd <pinedaclp@gmail.com>
2017-01-03 19:32:39 +0800

    update in job

--- src/pages/job/pages/job-components/job-header/job-header.ts
+++ src/pages/job/pages/job-components/job-header/job-header.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Member, MEMBER_LOGIN_DATA } from '../../../../../api/philgo-api/v2/member';
 import { AppRouter } from '../../../../../app/app.router';
+import { App } from "../../../../../providers/app";
 @Component ({
     selector: 'job-header',
     templateUrl: 'job-header.html'
@@ -10,13 +11,24 @@ export class JobHeader {
     @Input() title: string = '';
     login: MEMBER_LOGIN_DATA = <MEMBER_LOGIN_DATA> {};
     constructor(
+        public app: App,
         private member: Member,
         private router: AppRouter
     ) {
-        setTimeout(() => this.login = this.member.logged(), 10);
+        //setTimeout(() => this.login = this.member.logged(), 10);
+        app.menu = false;
+        member.getLogin( x => this.login = x );
+    }
+
+    onClickHeader() {
+        window.scrollTo( 0, 0 );
     }
     onClickLogout() {
+        this.login = null;
         this.member.logout();
-        this.router.go('/');
+    }
+
+    onClickMenu() {
+        this.app.menu = ! this.app.menu;
     }
 }
