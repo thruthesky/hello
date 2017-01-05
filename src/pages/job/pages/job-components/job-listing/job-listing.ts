da9437553c1752d12b9e46e994bc1268f030aa51
lancelynyrd <pinedaclp@gmail.com>
2017-01-05 14:39:15 +0800

    updating listing component on job

--- src/pages/job/pages/job-components/job-listing/job-listing.ts
+++ src/pages/job/pages/job-components/job-listing/job-listing.ts
@@ -2,11 +2,10 @@
  * @see ./README.md
  */
 import { Component, Input } from '@angular/core';
-import { POST_DATA } from '../../../../../api/philgo-api/v2/philgo-api-interface';
+import { POST_DATA, PAGES } from '../../../../../api/philgo-api/v2/philgo-api-interface';
 import { Post } from '../../../../../api/philgo-api/v2/post';
 import { MEMBER_LOGIN } from '../../../../../api/philgo-api/v2/member';
 import { Router } from '@angular/router';
-import * as _ from 'lodash';
 @Component({
   selector: 'job-listing',
   templateUrl: 'job-listing.html',
@@ -18,26 +17,21 @@ export class JobListing {
 
   today = new Date();
   currentYear = this.today.getFullYear();
-
-  //@Input() show: boolean = false; // if set true, the create/edit form box shows.
-  //@Input() mode: string = null;
-  @Input() post: POST_DATA = null; // it is comment or post.
-  @Input() shareUrl: string = null; // it is comment or post.
+  @Input() mode : string = 'posts';
+  @Input() pages : PAGES = null;
   @Input() login: MEMBER_LOGIN = {
     id: ''
-  }; // it is comment or post.
+  };
   @Input() root = [];
 
   //active: boolean = false; // "active==true" means, the use is in editing.
 
   constructor(private router: Router,private _post: Post,) {
-    console.log("ViewComponent()");
+    console.log("JobListing()");
   }
 
   ngOnInit() {
-    if (this.post === null) return alert("View Component Error: post is null");
-    //this.isPost = this.post.idx_parent == '0';
-    //this.isComment = !this.isPost;
+    if (this.pages === null) return alert("Listing Component Error: post is null");
   }
 
 
@@ -47,16 +41,13 @@ export class JobListing {
 
 
   onClickDelete( post ) {
+    console.log(post);
+    post.idx = null;
     let re = confirm("Are you sure you want to delete this post?");
     if ( re ) {
       this._post.delete( post.idx, re => {
           alert("Successful on Deleting this post...");
-
-          let index =  _.findIndex( this.root, data =>{
-            return data.idx == post.idx;
-          } );
-
-          this.root.splice(index, 1);
+          post.idx = null;
         },
         error => alert("delete error: " + error )
       );
