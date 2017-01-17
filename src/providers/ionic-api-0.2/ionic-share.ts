b803f92d1c3efbc8e3a5a0c2c474f47e6a276661
thruthesky <thruthesky@gmail.com>
2017-01-18 01:51:12 +0800

    Updating ionic app id.

---
+++ src/providers/ionic-api-0.2/ionic-share.ts
@@ -0,0 +1,9 @@
+export const IONIC_APP_NAME: string = "sonub";
+export const IONIC_PROFILE_TAG_NAME: string = "sonub";
+export const IONIC_PUSH_SENDER_ID: string = "130954267633";
+export const IONIC_PUSH_API_TOKEN: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiZWE5NTNkZi0zYWFjLTQxM2QtYWU1Yy1iZDk0ZTBjNjgzYjIifQ._JCU4AsRSOudwsf2EM-8uR_yT_etkRzx97u9Bfs7d2Q";
+export interface IONIC_PUSH_MESSAGE {
+    token: string;
+    title: string;
+    content: string;
+};
