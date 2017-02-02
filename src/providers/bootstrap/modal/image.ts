5623df32271b5485976f60b60a07448d88f658af
lancelynyrd <pinedaclp@gmail.com>
2017-02-02 23:05:07 +0800

    showing and hiding post

--- src/providers/bootstrap/modal/image.ts
+++ src/providers/bootstrap/modal/image.ts
@@ -3,16 +3,8 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'image-modal',
   template: `
-    <div class="modal-header">
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
     <div class="modal-body">
-      <img [src]="url" />
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" (click)="activeModal.close('Close click')">Close</button>
+      <img [src]="url" (click)="activeModal.close('Close click')" />
     </div>
     `
 })
